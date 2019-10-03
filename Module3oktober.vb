170c87bc7760af4365a5430c4f7a77cb7866cd00
kukuhyudhistiro <kukuh.yudhistiro@gmail.com>
2019-10-03 13:59:25 +0700

    Materi cek tipe data variabel dan logika IF
    Materi cek tipe data variabel dan logika IF

---
+++ Module3oktober.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        tesgenapganjil()
+    End Sub
+
+    Sub tesgenapganjil()
+        Dim bil As Integer
+        Dim sts As String
+        '1. MASUKKAN bil    
+        '2. JIKA SISA HASIL BAGI 2 DARI BIL ADALAH 0, MAKA  
+        '   STATUS GENAP, SELAIN ITU STATUS GANJIL	
+        '3. TAMPILKAN STATUS    
+        bil = 5
+        If (bil Mod 2) = 0 Then
+            sts = "GENAP"
+        Else
+            sts = "GANJIL"
+        End If
+        WriteLine(sts)
+    End Sub
+
+    Sub cekvariabel()
+        'PEMILIHAN TIPE DATA SANGAT PENTING:
+        ' - ketelitian - akurasi
+
+        Dim a As Single
+        Dim b As Single
+        Dim hasil As Integer
+        a = 2.5
+        b = 2
+        hasil = a + b
+
+        Write(hasil)
+        WriteLine()
+    End Sub
+
+End Module
