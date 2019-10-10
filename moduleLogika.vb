7abe69481f51124d682452629fd5656eefddad56
kukuhyudhistiro <kukuh.yudhistiro@gmail.com>
2019-10-10 13:50:41 +0700

    latihan logika AND OR
    latihan logika AND OR

---
+++ moduleLogika.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Console
+Module moduleLogika
+    Sub Main()
+        aplikasi5a()
+    End Sub
+
+    Sub cekkelulusanLOGIKA()
+        'LULUS jika:
+        'nilaiuas > 90 OR nilaiuts>70 AND nilaitugas > 80
+
+    End Sub
+
+    Sub aplikasi5a()
+        '1. siapkan variabel: status, nilai
+        Dim status As String
+        Dim nilai As Byte
+
+        '2. input nilai
+        Write("Masukkan nilai   = ")
+        nilai = ReadLine()
+        '3. cek nilai (jika nilai > 60, maka lulus)
+        If nilai > 60 Then
+            status = "LULUS"
+        Else
+            status = "TIDAK LULUS"
+        End If
+        '4. output status
+        WriteLine("STATUS ANDA      = " & status)
+        WriteLine()
+        WriteLine()
+
+    End Sub
+End Module
